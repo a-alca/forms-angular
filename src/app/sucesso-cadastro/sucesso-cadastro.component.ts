cb1ae7aa6296037ad0c04f097515677152248917
André Alcântara <106185003+a-alca@users.noreply.github.com>
2024-03-17 21:31:48 -0300

    import FormsModule e inclusao de pagina de sucesso

--- src/app/sucesso-cadastro/sucesso-cadastro.component.ts
+++ src/app/sucesso-cadastro/sucesso-cadastro.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sucesso-cadastro',
   templateUrl: './sucesso-cadastro.component.html',
   styleUrls: ['./sucesso-cadastro.component.css']
 })
-export class SucessoCadastroComponent {
+export class SucessoCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
